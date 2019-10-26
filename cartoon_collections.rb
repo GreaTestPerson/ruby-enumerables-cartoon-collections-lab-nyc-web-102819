ac19ea67a097e9cf60462fa89340198805662c16
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-26 17:56:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,13 +5,12 @@ def roll_call_dwarves(dwarves)
   }
 end
 
-def summon_captain_planet(array)
+def summon_captain_planet(planeteer_calls)
   # Your code here
-  if array.any? {|n| n.length > 4}
-    return true
-  else
-    return false
-  end
+  planeteer_calls = planeteer_calls.map {|food|
+  "#{food.capitalize}!"
+}
+
 end
 
 def long_planeteer_calls(array)
